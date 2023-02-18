1a0c8551c12096f5c7c0387852c055a32fe2c901
Andy Linnell <andylinnell.cafo@gmail.com>
2023-02-18 14:27:28 -0500

    Created API for tonnflix

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from "firebase-functions"
 import express from "express"
 import cors from "cors"
-import { getAllShows, addShow, updateShow, deleteShow } from "./src/shows.js"
+import { getAllShows, addShow } from "./src/shows.js"
 
 const app = express()
 app.use(cors())
@@ -9,8 +9,11 @@ app.use(express.json())
 
 app.get('/shows', getAllShows)
 app.post('/shows', addShow)
-app.patch('/shows/:showId', updateShow)
-app.delete('/shows/:showId', deleteShow)
+// app.patch('/shows/:showId', updateShow)
+// app.delete('/shows/:showId', deleteShow)
+app.get('/', (req, res) => {
+    res.send('toon flix')
+})
 
 
 export const api = functions.https.onRequest(app)
--- functions/src/shows.js
+++ functions/src/shows.js
@@ -4,7 +4,7 @@ import { FieldValue } from "firebase-admin/firestore";
 export async function getAllShows(req, res) {
     const db = await getFirestoreInstance();
 
-    db.collection('shows').orderBy('createdAt', 'desc').get()
+    db.collection('shows').get()
         .then(collection => {
             const shows = collection.docs.map(doc => ({ showId: doc.id, ...doc.data() }));
             res.send(shows);
@@ -13,8 +13,9 @@ export async function getAllShows(req, res) {
 }
 
 export async function addShow(req, res) {
-    const { show } = req.body;
-    const newShow = { show, createdAt: FieldValue.serverTimestamp() }
+    const show = req.body;
+    const newShow = { ...show, createdAt: FieldValue.serverTimestamp() }
+    console.log("------- ADDING: --------", newShow)
     const db = await getFirestoreInstance();
     db.collection('shows').add(newShow)
         .then(() => getAllShows(req, res)) // instantly sends back change and shows with change, skips a whole step of front end calling for information after change
@@ -22,25 +23,25 @@ export async function addShow(req, res) {
 }
 
 
-export async function deleteShow(req, res) {
-    const { showId } = req.params;
-    const db = await getFirestoreInstance();
+// export async function deleteShow(req, res) {
+//     const { showId } = req.params;
+//     const db = await getFirestoreInstance();
 
-    db.collection('shows')
-        .doc(showId)
-        .delete()
-        .then(() => getAllShows(req, res))
-        .catch(err => res.status(500).send({ error: err.message }));
-}
+//     db.collection('shows')
+//         .doc(showId)
+//         .delete()
+//         .then(() => getAllShows(req, res))
+//         .catch(err => res.status(500).send({ error: err.message }));
+// }
 
-export async function updateShow(req, res) {
-    const { show } = req.body;
-    const { showId } = req.params;
-    const db = await getFirestoreInstance();
+// export async function updateShow(req, res) {
+//     const { show } = req.body;
+//     const { showId } = req.params;
+//     const db = await getFirestoreInstance();
 
-    db.collection('shows')
-        .doc(showId)
-        .update({ show })
-        .then(() => getAllShows(req, res))
-        .catch(err => res.status(500).send({ error: err.message }));
-}
+//     db.collection('shows')
+//         .doc(showId)
+//         .update({ show })
+//         .then(() => getAllShows(req, res))
+//         .catch(err => res.status(500).send({ error: err.message }));
+// }
